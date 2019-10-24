227e87266ea7e86e52e9cbdd32a44acf1cda4a16
terrycojones <terry@jon.es>
2019-10-24 10:56:31 +0200

    Added 09-hamm code comment

--- 09-hamm.py
+++ 09-hamm.py
@@ -2,6 +2,9 @@
 # s2 = 'CCTATATGCATATACTT'
 
 with open('09-hamm.txt') as fp:
+    # The strip() isn't needed here, if both strings end with a
+    # newline. But it's possible the input file doesn't have a newline on
+    # its final line, so we should be careful.
     s1 = fp.readline().strip()
     s2 = fp.readline().strip()
 
